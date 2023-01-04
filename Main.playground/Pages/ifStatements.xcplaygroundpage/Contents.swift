3108d49074bad68624d26653790279bffd446d2a
Tom Munyiri <tmmunyiri@gmail.com>
2023-01-04 12:17:16 +0300

    try to organize code

---
+++ Main.playground/Pages/ifStatements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+import UIKit
+
+ifStatements()
+
+func ifStatements(){
+  let a=10
+  //condition => you can use parenthesis or choose not to
+  if a > 10 || a==10{
+    print("Number is greater than or equal to 10")
+  }else{
+    print("Number is not greater than 10")
+  }
+}
